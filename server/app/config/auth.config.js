aba5a89c72e78f91e2867722fa90a8b6b19a9fbc
mehtaanand35 <anandmehta237@gmail.com>
2022-04-08 15:39:01 +0530

    done

---
+++ server/app/config/auth.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  secret: "anand-secret-key",
+  user: "mehtaanand358@gmail.com", // put the mail id from which you want to get confirmation mail
+  pass: "", // put the password
+};
